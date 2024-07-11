ddfd30b10cce8c4630695190b29dad60d484e8ef
Tridominous <jamesmou02@gmail.com>
2024-07-11 16:13:30 +0100

    Corrected mongodbUserId prop

--- app/(root)/add-equipment/page.tsx
+++ app/(root)/add-equipment/page.tsx
@@ -16,7 +16,7 @@ const Page = async () => {
       <h1 className='h1-bold text-dark100_light900'>Add an Equipment</h1>
 
       <div className='mt-9'>
-        <Equipment mongoUserId={JSON.stringify(mongoUser._id)}/>
+        <Equipment mongoUserId={JSON.stringify(mongoUser?._id)}/>
       </div>
     </div>
   )

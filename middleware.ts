791b8a2ebf26eb6bffc62c9421bdbf7b248a9999
Tridominous <jamesmou02@gmail.com>
2024-07-12 11:49:57 +0100

    middleware.ts adjustment_1

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,31 @@
 
-import { clerkMiddleware } from '@clerk/nextjs/server';
+// import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({})
+// export default clerkMiddleware({})
+
+// export const config = {
+//   // The following matcher runs middleware on all routes
+//   // except static assets.
+//   matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+
+
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher([
+  '/add-equipment(.*)',
+  '/api/webhook(.*)',
+  '/tags(.*)',
+  '/community(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };

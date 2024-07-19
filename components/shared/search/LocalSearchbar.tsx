9a7f1643d3785f6e54808105b025d31d21651965
Tridominous <jamesmou02@gmail.com>
2024-07-19 15:02:07 +0100

    Set up the search functionality to use for the search features

--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { Input } from '@/components/ui/input'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils';
 
 interface CustomInputProps  {
     route: string;
@@ -18,6 +20,38 @@ const LocalSearchbar = ({
         placeholder,
         otherClasses
 }: CustomInputProps) => {
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    const query = searchParams.get('q');
+
+    const [search, setSearch] = useState(query ||"" );
+
+    useEffect(() => {
+        const delayDebouncedFn = setTimeout(() => {
+            if(search) {
+                const newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: 'q',
+                    value: search
+                })
+                router.push(newUrl, { scroll: false})
+            } else {
+                if(pathname === route) {
+                    const newUrl = removeKeysFromQuery({
+                        params: searchParams.toString(),
+                        keysToRemove: ['q']
+                    })
+                    router.push(newUrl, { scroll: false})
+                }
+            }
+        }, 300);
+
+        return () => clearTimeout(delayDebouncedFn);
+    }, [search, route, pathname, router, searchParams, query])
+
+
   return (
     <div>
         <div className={`background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}>
@@ -35,7 +69,7 @@ const LocalSearchbar = ({
                 type='text'
                 placeholder={placeholder}
              
-                onChange={() => {}}
+                onChange={(e) => setSearch(e.target.value)}
                 className='paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none'
             />
         </div>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import qs from "query-string";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -71,4 +72,40 @@ export function getJoinedDate(obj: DateObject): string {
   const date = new Date(obj.date);
   const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long' };
   return date.toLocaleDateString('en-US', options);
+}
+
+interface UrlQueryParams {
+  params: string;
+  key: string;
+  value: string | null;
+}
+
+export const formUrlQuery = ({params, key, value}: UrlQueryParams) => {
+  const currentUrl = qs.parse(params);
+  currentUrl[key] = value;
+
+  return qs.stringifyUrl({
+    url: window.location.pathname,
+    query: currentUrl,
+  },
+{skipNull: true})
+}
+
+interface RemoveUrlQueryParams {
+  params: string;
+  keysToRemove: string[];
+}
+
+export const removeKeysFromQuery = ({params, keysToRemove}: RemoveUrlQueryParams) => {
+  const currentUrl = qs.parse(params);
+  
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+    });
+
+  return qs.stringifyUrl({
+    url: window.location.pathname,
+    query: currentUrl,
+  },
+{skipNull: true})
 }
